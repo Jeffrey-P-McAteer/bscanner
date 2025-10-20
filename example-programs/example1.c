f8fb228c4e0bbd9fed33b4f1688d74883a5d7db3
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-10-20 10:37:39 -0600

    example1++

---
+++ example-programs/example1.c
@@ -0,0 +1,20 @@
+
+#include "stdio.h"
+
+int main(int argc, char** argv) {
+
+  if (argc < 1) {
+    printf("Usage: example1 FILE.txt\n");
+    return 1;
+  }
+  else {
+    char* filename = argv[1];
+
+    printf("Reading %s\n", filename);
+    // TODO
+
+  }
+
+  return 0;
+}
+
